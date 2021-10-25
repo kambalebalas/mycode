3ab14ffb71f3d78f1d1371abd70bd628ff351dd0
Balasaheb Kambale <bnkw@ntrs.com>
2021-10-25 15:22:09 +0000

    learning about input function

---
+++ test.py
@@ -0,0 +1,24 @@
+
+
+def main():
+    # create a list called Ice-cream
+    icecream= ["flavors", "salty"]
+    # display Ice-cream
+    ##print(icecream)
+    # display northerntrust 
+    northerntrust =["Bala", "inderjeet","jon","Nikita","Steve","Vinod"]
+    
+   ## print(northerntrust)
+    
+    Num=99
+    icecream.append(Num)
+    
+    ##print(icecream)
+   
+    Ntnum= input("What is your number?")
+    
+    Ntnum= int(Ntnum)
+
+    print(f"{Num} {icecream[0]} and {northerntrust[Ntnum]} chooses to be {icecream[1]}.")
+
+main()
