e6cd41ef439aba872559d24f70a5fcab15855500
Balasaheb Kambale <bnkw@ntrs.com>
2021-10-26 09:01:48 +0000

    join and split

---
+++ tuesdaychallenge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def main():
+
+    nums= [1,2,3,4,5]
+    words= {"verb": "sprint",
+            "adjective": "awesome",
+            "noun": "Python" }
+
+    name= input("What is your name?\n>")
+
+    # Hi <name>! Welcome to Day 2 of Python Training!
+    print("Hi " + " name.upper() " + "! Welcome to Day " + str[nums[1]] + " of " + words[noun] + " Training!")
+
+# unindented this line-- the function call was being defined as part of the function
+main()
+
