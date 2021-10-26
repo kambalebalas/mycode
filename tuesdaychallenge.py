511a97d4a3677dc54a446130b8af2b02f65476cc
Balasaheb Kambale <bnkw@ntrs.com>
2021-10-26 09:10:45 +0000

    tuesday rectify script

--- tuesdaychallenge.py
+++ tuesdaychallenge.py
@@ -10,7 +10,7 @@ def main():
     name= input("What is your name?\n>")
 
     # Hi <name>! Welcome to Day 2 of Python Training!
-    print("Hi " + " name.upper() " + "! Welcome to Day " + str[nums[1]] + " of " + words[noun] + " Training!")
+    print("Hi " + name.capitalize()   + "! Welcome to Day " + str(nums[1]) + " of " + words["noun"] + " Training!")
 
 # unindented this line-- the function call was being defined as part of the function
 main()
