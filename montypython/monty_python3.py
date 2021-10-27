a8dd0843f856b306f13290bbdcec39fdc9f6d4f5
Balasaheb Kambale <bnkw@ntrs.com>
2021-10-27 10:27:19 +0000

    Looping techniques

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -5,7 +5,7 @@
 round = 0
 answer = " "
 
-while round < 3 and answer != "Brian":
+while round < 3 and answer.capitalize() != "Brian":
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
     if answer == "Brian": # logic to check if user gave correct answer
