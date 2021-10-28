a6e2561ed7b91e6a991a6872ce812fd304479562
Balasaheb Kambale <bnkw@ntrs.com>
2021-10-28 10:03:00 +0000

    Learning to archive files

---
+++ fact/name.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+classinfo = {"all": 
+    [
+        {
+            "First": "Bala",
+            "Last": "Kambale",
+            "Skill Level": "G.O.A.T.",
+            "Spirit Animal": "zebra",
+            "Super Power": "Mind control",
+        },
+        {
+            "First": "Inderjeet",
+            "Last": "Khalsa",
+            "Skill Level": "astonishing",
+            "Spirit Animal": "seal",
+            "Super Power": "Technopathy",
+        },
+        {
+            "First": "Jon",
+            "Last": "Whiteside",
+            "Skill Level": "stunning",
+            "Spirit Animal": "ram",
+            "Super Power": "Levitation",
+        },
+        {
+            "First": "Nikita",
+            "Last": "Nikale",
+            "Skill Level": "wondrous",
+            "Spirit Animal": "otter",
+            "Super Power": "Psychic",
+        },
+        {
+            "First": "Steve",
+            "Last": "Spike",
+            "Skill Level": "breathtaking",
+            "Spirit Animal": "lion",
+            "Super Power": "Portal creation",
+        },
+        {
+            "First": "Vinod",
+            "Last": "Singh",
+            "Skill Level": "wonderful",
+            "Spirit Animal": "badger",
+            "Super Power": "Immortal",
+        },
+    ]
+}
+
+
+for name in clasinfo:
+    print("My name is ",name["First"] + " and my spirit animal is  ", + name["Spirit Animal"])
+    print("My name is ", name["First"] + " and my skills are   ", + name["Skill Level]")
+    print("My name is ", name["First"] + " and my super power is  ", + name["Super Powere"])
